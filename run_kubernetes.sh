be2ddd9c8a28db3d30ea44e036edf7f95de691b5
Andre Noberto <andrenoberto@protonmail.com>
2021-07-10 23:19:20 +0100

    Added log output and sleep to wait until pod is finished

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,12 +8,14 @@ dockerpath="andrenoberto/ml-microservice-api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run ml-microservice-api --image=$dockerpath --generator=run-pod/v1
+kubectl run ml-microservice-api --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 
 # Step 4:
+# Wait for pods to be created
+sleep 60
 # Forward the container port to a host
 kubectl port-forward ml-microservice-api 8000:80
