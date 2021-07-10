20507cbeb3f8383430a48c9387ff8bdd6a019af7
Andre Noberto <andrenoberto@protonmail.com>
2021-07-10 22:58:42 +0100

    Configured kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="andrenoberto/ml-microservice-api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservice-api --image=$dockerpath --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-microservice-api 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="andrenoberto/ml-microservice-api"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml-microservice-api $dockerpath
+docker login --username andrenoberto
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
